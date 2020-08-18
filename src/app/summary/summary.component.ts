34ab48719e967cf9facefa9f61fe113948126005
HANA-BOOK\smile <smilecana@gmail.com>
2020-08-17 23:48:55 -0300

    Added table  for transaction history.

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 export interface Tile {
   color: string;
@@ -6,7 +6,25 @@ export interface Tile {
   rows: number;
   text: string;
 }
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
@@ -14,12 +32,82 @@ export interface Tile {
 })
 
 export class SummaryComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
   public tiles: Tile[] = [
     {text: 'One', cols: 2, rows: 2, color: 'lightblue'},
     {text: 'Two', cols: 2, rows: 2, color: 'lightgreen'}
   ];
   public view: any[] = [500, 300];
 
+  public multi = [
+    {
+      name: 'Germany',
+      series: [
+        {
+          name: '1990',
+          value: 62000000
+        },
+        {
+          name: '2010',
+          value: 73000000
+        },
+        {
+          name: '2011',
+          value: 89400000
+        }
+      ]
+    },
+
+    {
+      name: 'USA',
+      series: [
+        {
+          name: '1990',
+          value: 250000000
+        },
+        {
+          name: '2010',
+          value: 309000000
+        },
+        {
+          name: '2011',
+          value: 311000000
+        }
+      ]
+    },
+
+    {
+      name: 'France',
+      series: [
+        {
+          name: '1990',
+          value: 58000000
+        },
+        {
+          name: '2010',
+          value: 50000020
+        },
+        {
+          name: '2011',
+          value: 58000000
+        }
+      ]
+    },
+    {
+      name: 'UK',
+      series: [
+        {
+          name: '1990',
+          value: 57000000
+        },
+        {
+          name: '2010',
+          value: 62000000
+        }
+      ]
+    }
+  ];
 
   public single = [
     {
@@ -55,6 +143,16 @@ export class SummaryComponent implements OnInit {
   public showLegend = true;
   public showLabels = true;
   public isDoughnut = false;
+  public legend = true;
+  public animations = true;
+  public xAxis = true;
+  public yAxis = true;
+  public showYAxisLabel = true;
+  public showXAxisLabel = true;
+  public xAxisLabel = 'Year';
+  public yAxisLabel = 'Population';
+  public timeline = true;
+
   constructor() {
   }
 
@@ -71,6 +169,6 @@ export class SummaryComponent implements OnInit {
   }
 
   ngOnInit(): void {
-  }
+  };
 
 }
